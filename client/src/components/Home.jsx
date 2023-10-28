2f707d00aaec1b358d45c5605d387094c8cbae9f
michellemwangi01 <michellemwangim@gmail.com>
2023-10-28 07:59:01 +0300

    loaded categories from category API on homepage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import { dataContext } from "../contextProvider/DataContextProvider";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { categories } = useContext(dataContext);
+
+  console.log(categories);
+
+  const categoriesList = categories.map((category) => (
+    <img src={`${category.image}`} alt="" key={category.id} />
+  ));
+
+  console.log(categoriesList);
+  return (
+    <div>
+      <h1>Home</h1>
+      {categoriesList}
+    </div>
+  );
 };
 
 export default Home;
--- client/src/contextProvider/DataContextProvider.jsx
+++ client/src/contextProvider/DataContextProvider.jsx
@@ -1,13 +1,28 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
 
 const dataContext = createContext();
 
 const DataContextProvider = ({ children }) => {
-  const [groupMembers, setGroupMembers] = useState(["Donell"]);
+  const [categories, setCategories] = useState([]); //create your state variable
+  const [loadingCategories, setLoadingCategories] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:5555/farmart/categories")
+      .then((res) => {
+        setCategories(res.data);
+        setLoadingCategories(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching categories:", error);
+        setLoadingCategories(false);
+      });
+  }, []);
 
   const data = {
-    groupMembers,
-    setGroupMembers,
+    categories, //add it to the list of data items to be passed to all components
+    setCategories,
   };
   return <dataContext.Provider value={data}>{children}</dataContext.Provider>;
 };
