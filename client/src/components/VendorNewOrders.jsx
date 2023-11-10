c5dc45fcf07eb311010fb31a03b297d902134a2e
michellemwangi01 <michellemwangim@gmail.com>
2023-11-10 10:04:32 +0300

    added delete product by farmer

--- client/src/components/VendorNewOrders.jsx
+++ client/src/components/VendorNewOrders.jsx
@@ -164,14 +164,14 @@ const VendorNewOrders = () => {
               View Order Details
             </p>
             <button
-              onClick={() => approveOrderHandlerHome(orderItem.id)}
+              onClick={() => approveOrderHandlerHome(orderItem.order_id)}
               type="button"
               class=" text-base focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
             >
               Approve
             </button>
             <button
-              onClick={() => rejectOrderHandlerHome(orderItem.id)}
+              onClick={() => rejectOrderHandlerHome(orderItem.order_id)}
               type="button"
               class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-sm text-base px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
             >
